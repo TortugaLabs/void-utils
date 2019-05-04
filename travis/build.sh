84231ad5bde1f5e45c6377261c7e1491ea16ff7a
Alejandro Liu <alejandro_liu@hotmail.com>
2019-05-04 22:08:33 +0200

    13680

---
+++ travis/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# build.sh
+
+if [ "$1" != "$2" ]; then
+  arch="-a $2"
+fi
+
+set -x
+ls -l
+pwd
+type gcc
+
+
+exit 0
+
+# Tell xbps-src what is our arch, this is done when doing
+# binary-bootstrap, but we need to do it every time since
+# our masterdir is ethereal.
+# /bin/echo -e '\x1b[32mWriting bootstrap arch into .xbps_chroot_init of masterdir\x1b[0m'
+# echo "$1" > /hostrepo/masterdir/.xbps_chroot_init
+
+/bin/echo -e '\x1b[32mPreparing chroot with chroot_prepare()\x1b[0m'
+source hostrepo/common/xbps-src/shutils/chroot.sh || {
+	echo "Failed to source chroot.sh for chroot_prepare()" >&2 ;
+	exit 1
+}
+
+XBPS_SRCPKGDIR=/hostrepo/srcpkgs XBPS_MASTERDIR=/ chroot_prepare $1 || {
+	echo "Failed to prepare chroot!" >&2 ;
+	exit 1
+}
+
+PKGS=$(/hostrepo/xbps-src sort-dependencies $(cat /tmp/templates))
+
+for pkg in ${PKGS}; do
+	/hostrepo/xbps-src -H "$HOME"/hostdir $arch pkg "$pkg"
+	[ $? -eq 1 ] && exit 1
+done
+
+exit 0
